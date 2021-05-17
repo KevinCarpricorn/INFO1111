a5a1c931533ee94922eb998e4db4f024cde97532
Kevin <zhengyuxiang8888@gmail.com>
2021-05-18 00:26:53 +0800

    updata version(bug in networks)

--- Self_learning Level1-2/Fashion recognition.py
+++ Self_learning Level1-2/Fashion recognition.py
@@ -1,14 +1,18 @@
 import torch
-import torchvision
-import torchvision.transforms as transforms
 import torch.nn as nn
-from torch.utils.data import DataLoader
 import torch.optim as optim
 import torch.nn.functional as F
-import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
-from plot import plot_confusion_matrix
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from IPython.display import display, clear_output
+import pandas as pd
 import time
+import json
+from itertools import product
+from collections import namedtuple
+from collections import OrderedDict
 
 
 def get_num_correct(preds, labels):
@@ -83,55 +87,194 @@ class Network(nn.Module):
         return t
 
 
-# training model
+# Tensorboard set up
+tb = SummaryWriter()
+
 network = Network()
+images, labels = next(iter(train_loader))
+grid = torchvision.utils.make_grid(images)
+
+tb.add_image('images', grid)
+tb.add_graph(network, images)
+tb.close()
+
+
+class RunBuilder():
+    @staticmethod
+    def get_runs(params):
+
+        Run = namedtuple('Run', params.keys())
+
+        runs = []
+        for v in product(*params.values()):
+            runs.append(Run(*v))
+
+        return runs
+
 
 # He initialization
-for m in network.modules():
-    if isinstance(m, (nn.Conv2d, nn.Linear)):
-        nn.init.kaiming_normal_(m.weight, mode='fan_in')
+# for m in network.modules():
+#     if isinstance(m, (nn.Conv2d, nn.Linear)):
+#         nn.init.kaiming_normal_(m.weight, mode='fan_in')
 
-optimizer = optim.Adam(network.parameters(), lr=0.1)
 
-start = time.time()
-for epoch in range(20):
-    total_loss = 0
-    total_correct = 0
+class Epoch():
+    def __init__(self):
+        self.count = 0
+        self.loss = 0
+        self.num_correct = 0
+        self.start_time = None
 
-    for batch in train_loader:
-        images, labels = batch
 
-        preds = network(images)
-        loss = F.cross_entropy(preds, labels)
+e = Epoch()
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
 
-        total_loss += loss.item()
-        total_correct += get_num_correct(preds, labels)
+class RunManager():
+    def __init__(self):
 
-    print(f'epoch: {epoch + 1}, total correct: {total_correct}, loss: {total_loss}, Accuracy: {total_correct / len(train_set)}')
-end = time.time()
-print(f"time: {end - start}")
+        self.epoch_count = 0
+        self.epoch_loss = 0
+        self.epoch_num_correct = 0
+        self.epoch_start_time = None
+
+        self.run_params = None
+        self.run_count = 0
+        self.run_data = []
+        self.run_start_time = None
+
+        self.network = None
+        self.loader = None
+        self.tb = None
+
+    def begin_run(self, run, network, loader):
+
+        self.run_start_time = time.time()
+
+        self.run_params = run
+        self.run_count += 1
+
+        self.network = network
+        self.loader = loader
+        self.tb = SummaryWriter(comment=f'-{run}')
+
+        images, labels = next(iter(self.loader))
+        grid = torchvision.utils.make_grid(images)
+
+        self.tb.add_image('images', grid)
+        self.tb.add_graph(self.network, images)
+
+    def end_run(self):
+        self.tb.close()
+        self.epoch_count = 0
+
+    def begin_epoch(self):
+        self.epoch_start_time = time.time()
+
+        self.epoch_count += 1
+        self.epoch_loss = 0
+        self.epoch_num_correct = 0
+
+    def end_epoch(self):
+
+        epoch_duration = time.time() - self.epoch_start_time
+        run_duration = time.time() - self.run_start_time
+
+        loss = self.epoch_loss / len(self.loader.dataset)
+        accuracy = self.epoch_num_correct / len(self.loader.dataset)
+
+        self.tb.add_scalar('Loss', loss, self.epoch_count)
+        self.tb.add_scalar('Accuracy', accuracy, self.epoch_count)
+
+        for name, param in self.network.named_parameters():
+            self.tb.add_histogram(name, param, self.epoch_count)
+            self.tb.add_histogram(f'{name}.grad', param.grad, self.epoch_count)
+
+        results = OrderedDict()
+        results["run"] = self.run_count
+        results["epoch"] = self.epoch_count
+        results['loss'] = loss
+        results["accuracy"] = accuracy
+        results['epoch duration'] = epoch_duration
+        results['run duration'] = run_duration
+        for k,v in self.run_params._asdict().items(): results[k] = v
+        self.run_data.append(results)
+
+        df = pd.DataFrame.from_dict(self.run_data, orient='columns')
+
+        clear_output(wait=True)
+        display(df)
+
+    def track_loss(self, loss):
+        self.epoch_loss += loss.item() * self.loader.batch_size
+
+    def track_num_correct(self, preds, labels):
+        self.epoch_num_correct += self._get_num_correct(preds, labels)
+
+    @torch.no_grad()
+    def _get_num_correct(self, preds, labels):
+        return preds.argmax(dim=1).eq(labels).sum().item()
+
+    def save(self, fileName):
+
+        pd.DataFrame.from_dict(
+            self.run_data, orient='columns'
+        ).to_csv(f'{fileName}.csv')
+
+        with open(f'{fileName}.json', 'w', encoding='utf-8') as f:
+            json.dump(self.run_data, f, ensure_ascii=False, indent=4)
+
+
+parameters = OrderedDict(
+    lr = [0.1, 0.01, 0.001],
+    batch_size = [10, 100, 1000],
+    shuffle = [True, False]
+)
+
+m = RunManager()
+for run in RunBuilder.get_runs(parameters):
+
+    network = Network()
+    loader = DataLoader(train_set, batch_size=run.batch_size, shuffle=run.shuffle)
+    optimizer = optim.Adam(network.parameters(), lr=run.lr)
+
+    m.begin_run(run, network, loader)
+
+    for epoch in range(5):
+        m.begin_epoch()
+        for batch in train_loader:
+
+            images = batch[0]
+            labels = batch[1]
+            preds = network(images)
+            loss = F.cross_entropy(preds, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            m.track_loss(loss)
+            m.track_num_correct(preds, labels)
+
+        m.end_epoch()
+    m.end_run()
+m.save('results')
 
 
 # testing "plotting a confusion matrix"
-@torch.no_grad()
-def get_all_preds(model, loader):
-    all_preds = torch.tensor([])
-    for batch in loader:
-        images, labels = batch
-
-        preds = model(images)
-        all_preds = torch.cat((all_preds, preds), dim=0)
-
-    return all_preds
-
-
-test_loader = DataLoader(test_set, batch_size=100)
-test_preds = get_all_preds(network, test_loader)
-cm = confusion_matrix(test_set.targets, test_preds.argmax(dim=1))
-plt.figure(figsize=(10, 10))
-plot_confusion_matrix(cm, test_set.classes)
-plt.show()
+# @torch.no_grad()
+# def get_all_preds(model, loader):
+#     all_preds = torch.tensor([])
+#     for batch in loader:
+#         images, labels = batch
+#
+#         preds = model(images)
+#         all_preds = torch.cat((all_preds, preds), dim=0)
+#
+#     return all_preds
+#
+#
+# test_loader = DataLoader(test_set, batch_size=100)
+# test_preds = get_all_preds(network, test_loader)
+# cm = confusion_matrix(test_set.targets, test_preds.argmax(dim=1))
+# plt.figure(figsize=(10, 10))
+# plot_confusion_matrix(cm, test_set.classes)
+# plt.show()
